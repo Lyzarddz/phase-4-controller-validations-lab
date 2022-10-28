8444bccee7988506375e2b96f0cdb4e93d88619b
Alyssa Falcione <lyzarddz@gmail.com>
2022-10-28 15:21:45 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,11 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)     #must have ! after update
     render json: post
+#Solution code
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -19,5 +20,4 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
